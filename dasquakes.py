1c3ad65d2e2fa6373c55df9a3f1c8a2b0a0cc3ed
Parker Sprinkle <dpsprink@gmail.com>
2022-12-09 15:31:47 -0800

    add fiber location function to dasquakes

--- dasquakes.py
+++ dasquakes.py
@@ -263,3 +263,68 @@ def fk_analysis(t0, draw_figure = True,downsamplefactor=5,cable = 'whidbey', rec
     k = fftshift(fftfreq(downsampled_subsea_data.shape[1], d=attrs['SpatialSamplingInterval']))
     
     return ft,f,k
+
+import geopy.distance
+from shapely.geometry import LineString
+
+#For sintela fiberroute and calibration files
+def fiber_channel_locator(fiber_file = 'fiberroute.csv', cal_file='calibration.csv', chan_spac = 6.3, num_chans = 1750):
+    fiber_location = pd.read_csv(fiber_file, header=1)
+    fiber_calibration = pd.read_csv(cal_file, header=1)
+    fiber_distance = []
+    opt_dis_merge = []
+    chan_num = []
+
+    for index_fib, row_fib in fiber_location.iterrows():
+        if index_fib == 0 :
+            fiber_distance.append(0)
+        elif index_fib > 0:
+            coords_1 = (row_fib['Lat'], row_fib['Long'])
+            coords_2 = (fiber_location.iloc[index_fib-1]['Lat'], fiber_location.iloc[index_fib-1]['Long'])
+            distance = geopy.distance.geodesic(coords_1, coords_2).m
+            fiber_distance.append(distance   + fiber_distance[-1])
+
+        l = []
+        for index_cal, row_cal in fiber_calibration.iterrows():
+            if row_fib['Lat'] == row_cal['Lat'] and row_fib['Long'] == row_cal['Long']:
+                l.append(row_cal['Opt Dist'])
+            else:
+                pass
+        if l:
+            opt_dis_merge.append(l[0])
+        else:
+            opt_dis_merge.append(np.nan)
+
+    fiber_location['Fiber Dist'] = fiber_distance
+    fiber_location['Opt Dist'] = opt_dis_merge
+    dis_interp = fiber_location['Opt Dist'].interpolate(method='linear', fill_value='extrapolate')
+    fiber_location['Opt Dist Interp'] = dis_interp
+    
+    for index_merge, row_merge in fiber_location.iterrows():
+        if row_merge['Opt Dist Interp'] != np.nan:
+            
+            chan_num.append(row_merge['Opt Dist Interp'] / chan_spac)
+            
+    fiber_location['Chan Num Interp'] = chan_num
+    
+    coords_of_chans_x = []
+    coords_of_chans_y = []
+
+
+    for index, values in fiber_location.iterrows():
+        if index < len(fiber_location) - 1:
+            #print(values['Lat'], fiber_location.iloc[index+1]['Lat'])
+            xy_floats = [(fiber_location.iloc[index]['Long'], fiber_location.iloc[index]['Lat']),
+                         (fiber_location.iloc[index+1]['Long'], fiber_location.iloc[index+1]['Lat'])]
+            line = LineString(xy_floats)
+            num_points = int(round((fiber_location.iloc[index+1]['Opt Dist Interp'] - values['Opt Dist Interp']) / attrs['SpatialSamplingInterval']))
+            new_points = [line.interpolate(i/float(num_points - 1), normalized=True) for i in range(num_points)]
+            xs = [point.x for point in new_points]
+            ys = [point.y for point in new_points]
+            coords_of_chans_x.append(xs)
+            coords_of_chans_y.append(ys)
+
+    flat_x =  [item for sublist in coords_of_chans_x for item in sublist]
+    flat_y =  [item for sublist in coords_of_chans_y for item in sublist]
+    
+    return fiber_location, flat_x, flat_y
